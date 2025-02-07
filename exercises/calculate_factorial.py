34f8d13dabe4499c1b4e46942d605d5ff1a4197c
CRK222 <chanarachel04@gmail.com>
2025-02-07 00:08:44 -0500

    Add code to calculate factorials (implement Module 2 exercise)

--- exercises/calculate_factorial.py
+++ exercises/calculate_factorial.py
@@ -1,15 +1,40 @@
-# implement factorial_recursive method
+# Implement factorial_recursive method
 def factorial_recursive(number):
-    pass
+    if number == 0:
+        return 1
+    else:
+        return number * factorial_recursive(number - 1)
+
+
+# Helper method to get and validate a non-negative integer
+def get_non_negative_integer():
+    while True:
+        try:
+            num = int(input("Enter a non-negative integer: "))
+            if num >= 0:
+                return num
+            else:
+                print("Your number is out of the correct range. Please try again.")
+        except ValueError:
+            print("Invalid entry. Please try again.")
+
+
+# Helper method to display the result of a factorial calculation
+def display_factorial_results(number, result):
+    print(f"The result of {number}! is {result}.")
 
 
 def main():
     print("Factorial Computation Using Recursion")
-    number = int(input("Enter a non-negative integer: "))
 
-    # handle negative input
+    # Get value to calculate, handle negative input
+    number = get_non_negative_integer()
 
     # Call factorial_recursive method
+    result = factorial_recursive(number)
+
+    # Display results of the factorial calculation
+    display_factorial_results(number, result)
 
 
 if __name__ == "__main__":
