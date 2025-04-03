4dc1e273c9990e98bb3849426b95d867f4d3daf8
CRK222 <chanarachel04@gmail.com>
2025-04-03 15:46:30 -0400

    Add files for Module3 (including myproject)

---
+++ myproject/learning_logs/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Topic, Entry
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date_created')
+    list_filter = ('date_created',)
+    search_fields = ('name', 'description')
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'title', 'content', 'date_created')
+    list_filter = ('date_created',)
+    search_fields = ('title', 'content')
